b79fefb3e38e46e6e6a73aac8a4c61a10aac01ca
Jairo Sanchez <jairosr_1995@hotmail.com>
2024-11-22 23:48:43 -0500

    Agregar funcionalidad para descargar archivos a una carpeta temporal y actualizar .gitignore (#3)

--- internal/downloader/downloader.go
+++ internal/downloader/downloader.go
@@ -1,5 +1,13 @@
 package downloader
 
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+)
+
 // HTTPDownloader implementa la interfaz Downloader utilizando HTTP.
 type HTTPDownloader struct{}
 
@@ -19,5 +27,37 @@ func NewHTTPDownloader() *HTTPDownloader {
 func (d *HTTPDownloader) Download(urls []string) ([]string, error) {
 	var files []string
 
+	// Crear la carpeta "temp" si no existe
+	err := os.MkdirAll("temp", os.ModePerm)
+	if err != nil {
+		return nil, fmt.Errorf("error al crear la carpeta temp: %v", err)
+	}
+
+	// Descargar cada archivo
+	for _, url := range urls {
+		fileName := filepath.Join("temp", filepath.Base(url))
+		outFile, err := os.Create(fileName)
+		if err != nil {
+			return nil, fmt.Errorf("error al crear el archivo %s: %v", fileName, err)
+		}
+		defer outFile.Close()
+
+		resp, err := http.Get(url)
+		if err != nil {
+			return nil, fmt.Errorf("error al descargar %s: %v", url, err)
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("error en la respuesta HTTP al descargar %s: %s", url, resp.Status)
+		}
+
+		_, err = io.Copy(outFile, resp.Body)
+		if err != nil {
+			return nil, fmt.Errorf("error al guardar el archivo %s: %v", fileName, err)
+		}
+
+		files = append(files, fileName)
+	}
 	return files, nil
 }
