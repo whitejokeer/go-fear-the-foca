0c181c344f6853a5df67f26e5c3a4b24ebd853ba
Jairo Sanchez <jairosr_1995@hotmail.com>
2024-11-23 01:33:47 -0500

    Agregar soporte para extraer metadatos de archivos PDF utilizando exiftool y actualizar dependencias en el Makefile (#4)

--- internal/metadata/pdf_extractor.go
+++ internal/metadata/pdf_extractor.go
@@ -1,5 +1,10 @@
 package metadata
 
+import (
+	"encoding/json"
+	"os/exec"
+)
+
 // PDFExtractor implementa la interfaz Extractor para archivos PDF.
 type PDFExtractor struct{}
 
@@ -19,6 +24,44 @@ func NewPDFExtractor() *PDFExtractor {
 //   - Un error si ocurre algún problema durante la extracción.
 func (p *PDFExtractor) Extract(files []string) ([]Metadata, error) {
 	var metadataList []Metadata
+
+	for _, file := range files {
+		// Construir el comando exiftool
+		cmd := exec.Command("exiftool", "-All", "-H", "-j", file)
+
+		// Ejecutar el comando y capturar la salida
+		output, err := cmd.Output()
+		if err != nil {
+			return nil, err
+		}
+
+		// Parsear la salida JSON
+		var metadataArray []map[string]interface{}
+		err = json.Unmarshal(output, &metadataArray)
+		if err != nil {
+			return nil, err
+		}
+
+		// Procesar los metadatos
+		if len(metadataArray) > 0 {
+			metadataMap := metadataArray[0]
+
+			// Extraer el nombre del archivo
+			fileName, _ := metadataMap["SourceFile"].(string)
+
+			// Remover SourceFile del mapa de propiedades
+			delete(metadataMap, "SourceFile")
+
+			// Crear una instancia de Metadata
+			metadata := Metadata{
+				FileName:   fileName,
+				Properties: metadataMap,
+			}
+
+			// Agregar a la lista de metadatos
+			metadataList = append(metadataList, metadata)
+		}
+	}
 
 	return metadataList, nil
 }
